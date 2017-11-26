63b4de20306d8fd253038ea9b28d7599c3bab884
ashtrus <zygimantas.butas@gmail.com>
2017-11-26 01:06:22 +0100

    merged with feature/getting-data-from-firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 import { LoginServiceProvider } from "../../providers/login-service/login-service";
 
@@ -9,14 +12,46 @@ import { LoginServiceProvider } from "../../providers/login-service/login-servic
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private loginService: LoginServiceProvider) {
+
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+
+    constructor(private afDB: AngularFireDatabase, private loginService: LoginServiceProvider, private navParams: NavParams) {
+
+    this.itemsRef = afDB.list(`items/${this.getUser().uid}`);
+    console.log(this.itemsRef);
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+
   }
 
-  getUser() {
+  // private getUsers() {
+  //   this.usersRef = this.afDB.list('users').valueChanges();
+  // }
+
+  private addItem(newName: string) {
+    this.itemsRef.push({ text: newName });
+  }
+
+  private updateItem(key: string, newText: string) {
+    this.itemsRef.update(key, { text: newText });
+  }
+
+  private deleteItem(key: string) {
+    this.itemsRef.remove(key);
+  }
+
+  private deleteEverything() {
+    this.itemsRef.remove();
+  }
+
+  private getUser() {
     return this.loginService.getUser();
   }
 
-  logOut() {
+  private logOut() {
     this.loginService.signOut();
   }
 }
