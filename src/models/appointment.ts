0add2e6b871bab6795656534f9e455c1ac7502f6
ashtrus <zygimantas.butas@gmail.com>
2017-12-09 12:37:30 +0100

    placeholders made for categories and featured appointments

---
+++ src/models/appointment.ts
@@ -0,0 +1,9 @@
+export class Appointment {
+  constructor(
+    public id: string,
+    public companyName: string,
+    public title: string,
+    public description: string,
+    public price: string,
+) { }
+}
